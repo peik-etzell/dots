3c13202d213064d64c26c3b31225d9ba3857c128
Peik Etzell <peik.etzell@gmail.com>
2023-06-07 21:31:03 +0300

    Log swaylock crashing

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 # https://unix.stackexchange.com/questions/591621/how-can-i-reload-swayidle-swaylock
-
-TIMEOUT=600
+TIMEOUT=50
 SLEEP_MODE=
 
 if [[ "$(cat /sys/power/mem_sleep | grep '\[s2idle\]')" != "" ]]; then
@@ -15,12 +14,16 @@ case "$1" in
 		pkill swaylock
 		swaylock -f --image "$WALLPAPER"
 		;;
+	lock-logged)
+		# pkill swaylock
+		(echo -e "\n\nStarting swaylock:\n"; WAYLAND_DEBUG=1 swaylock -c '000000' 2>&1 | sed  's/\[.......\....\]/\[XXXXXXX.YYY\]/') | grep -v keyboard | grep -v pointer >> ~/swaylock_logfile
+		;;
 	enable-lock)
 		pkill swayidle
 		swayidle -w \
-			timeout $(( TIMEOUT ))	"lock.sh lock" \
-			timeout $(( TIMEOUT * 1 ))	"systemctl $SLEEP_MODE" \
-			before-sleep				"lock.sh lock" &
+			timeout $(( TIMEOUT ))	"systemctl $SLEEP_MODE" \
+			before-sleep				"lock.sh lock-logged" &
 		;;
+			# timeout $(( TIMEOUT ))		"lock.sh lock-logged" \
 esac
 
