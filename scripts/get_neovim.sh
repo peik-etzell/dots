1d0cb2f463807815cede84f7dc1413d3837af0c3
Etzell Peik <etzellp1@emo.org.aalto.fi>
2023-10-24 16:48:59 +0300

    Add scripts to get neovim and lazygit binaries for non-root installs

---
+++ scripts/get_neovim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x ./nvim.appimage
+./nvim.appimage --appimage-extract
+ln -sv "$(pwd)/squashfs-root/usr/bin/nvim" $HOME/.local/bin
