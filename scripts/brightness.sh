109c4aaeb43f68a829f24f4cb64f4d25f129d2bf
Peik Etzell <peik.etzell@gmail.com>
2024-02-01 12:49:24 +0200

    Minimize bashisms

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-if [[ "$1" == "up" ]]; then
-	brillo -A 10
-    printf "%.0f\n" "$(brillo)" 2> /dev/null
-elif [[ "$1" == "down" ]]; then
-	brillo -U 10
-    printf "%.0f\n" "$(brillo)" 2> /dev/null
+if [ "$1" = "up" ]; then
+    brillo -A 5
+    printf "%.0f\n" "$(brillo)" 2>/dev/null
+elif [ "$1" = "down" ]; then
+    brillo -U 5
+    printf "%.0f\n" "$(brillo)" 2>/dev/null
 fi
-
