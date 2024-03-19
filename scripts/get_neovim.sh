b217c292b82e3a089ba26fd3537d531bde7fa94d
Peik Etzell <peik.etzell@gmail.com>
2024-03-19 19:57:20 +0200

    Use 9.5 in get_neovim, not nightly

--- scripts/get_neovim.sh
+++ scripts/get_neovim.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
 chmod u+x ./nvim.appimage
 ./nvim.appimage --appimage-extract
+rm nvim.appimage
 ln -sv "$(pwd)/squashfs-root/usr/bin/nvim" $HOME/.local/bin
