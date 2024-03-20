1e63f640672de494c8ee4603f4e4cacd36e34f4c
Peik Etzell <peik.etzell@gmail.com>
2024-03-20 12:55:20 +0200

    fix neovim script

--- scripts/get_neovim.sh
+++ scripts/get_neovim.sh
@@ -4,4 +4,5 @@ wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
 chmod u+x ./nvim.appimage
 ./nvim.appimage --appimage-extract
 rm nvim.appimage
+mkdir -p $HOME/.local/bin
 ln -sv "$(pwd)/squashfs-root/usr/bin/nvim" $HOME/.local/bin
